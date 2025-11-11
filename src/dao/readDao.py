ae9650d9fc7de5c982b2379b0f91ce96fe7475fe
Matchyman <jmatcham31@googlemail.com>
2025-11-11 15:51:10 +0000

    added join to readDao changed column names around in offers table

--- src/dao/readDao.py
+++ src/dao/readDao.py
@@ -19,3 +19,21 @@ class ReadDao:
         data = self.dao.get_data(con=connection, query=query)
         self.dao.close_connection()
         return data
+    
+    def get_item_and_offer(self, table:str, column:str, value:str):
+        connection = self.dao.connect()
+        query = f"""SELECT prices.code, prices.price, offers.amount, offers.offerprice 
+                    FROM {table} 
+                    LEFT JOIN offers 
+                    ON prices.code = offers.code
+                    WHERE {table}.{column} = '{value}'"""
+        data = self.dao.get_data(con=connection, query=query)
+        self.dao.close_connection()
+        return data
+    
+    
+def main():
+    ReadDao().get_item_and_offer(table='prices', column="code", value="A")
+    
+if __name__ == "__main__":
+    main()
